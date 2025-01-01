f0807a877e04b0ad905c7bc0b557683fb422a758
Brunexgmaer09 <39810796+Brunexgmaer09@users.noreply.github.com>
2025-01-01 11:00:56 -0300

    Delete Redeneural directory

--- Redeneural/Variaveis.cpp
+++
@@ -1,9 +0,0 @@
-#include "Variaveis.hpp"
-
-namespace Variaveis {
-    int GeracaoCompleta = 0;
-    std::vector<double> BestFitnessPopulacao;
-    std::vector<double> MediaFitnessPopulacao;
-    std::vector<double> MediaFitnessFilhos;
-    RedeNeural* MelhorRede = nullptr;
-} 
